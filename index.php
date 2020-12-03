a4faca22aee6c63245a57359e72ba6e5ba5c8e20
Matt <matthowe85@gmail.com>
2020-12-03 10:22:18 -0500

    push push baby

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 
 include "header.php";
 
+$page = $_GET['page'];
 ?>
 
 <div id="main"></div>
-<div id="projects"></div>
-<div id="about"></div>
+<?php if ($page == 'portfolio'){?>
+    <div id="portfolio">Portfolio</div>
+<?php } else if($page == 'about'){?>
+    <div id="about">about</div>
+<?php }  else if ($page == 'resume') {?>
+    <div id="resume">resume</div>
+<?php } ?>
 
 
 
