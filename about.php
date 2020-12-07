974a503ef398d3d0c3db379b14af362925b64b46
MATTHEW HOWE <matthewhowe@MATTHEWs-iMac.local>
2020-12-07 10:08:46 -0500

    added about me stuff

--- about.php
+++ about.php
@@ -12,11 +12,11 @@
 			<div class="row">
 				<div class="col-6">
 					<h1>About</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus ante auctor ligula accumsan iaculis. Ut vel ultricies nisi. Nam vitae tempor tortor, in laoreet nulla. Integer porttitor dignissim rutrum. In sodales interdum risus, congue pellentesque erat pulvinar id. Sed est eros, vestibulum ut nibh in, facilisis aliquam metus. Integer scelerisque mi non lectus placerat, eget aliquam felis faucibus. Aliquam a enim in ante malesuada finibus non eget purus. Cras in tortor eget odio placerat efficitur sed sit amet lectus. Maecenas sit amet ullamcorper magna, eu cursus augue. Donec sed tincidunt erat, eu hendrerit neque. Donec dignissim, lorem non facilisis efficitur, nunc nibh dapibus elit, sed maximus felis ante id ligula.</p>
+					<p>This website is obviously under construction but I doubt too many people are looking at it right now. It's coming along quite well though so check back soon! </p>
 
-					<p>Nullam volutpat mi sit amet diam lobortis, nec scelerisque augue sodales. Mauris eu blandit purus, quis aliquam dui. Quisque a nulla at augue mollis fringilla a et tortor. Nam imperdiet pulvinar risus, ultrices vulputate ipsum facilisis gravida. Donec quis vestibulum mi. In lobortis bibendum molestie. Sed at aliquam velit. Sed hendrerit nulla a quam tempus, et fermentum nulla congue. Proin sollicitudin nunc hendrerit, congue justo eget, consectetur ante. Vivamus mollis hendrerit ipsum, eu consectetur metus molestie convallis. Aliquam dignissim odio non molestie vestibulum.</p>
+					<p>I was in the vending industry for 12 years before and I wanted to change careers. In 2016 when I decided to begin my web development journey by taking online classes for web development. In 2018 I obtained my first position as a Front-End Web Developer and have been in love with all things web development ever since! </p>
 
-					<p>Ut rutrum dui purus, vel placerat erat sollicitudin quis. Cras sed semper quam. Ut lobortis massa vitae augue malesuada, eu ornare lectus vestibulum. Donec fringilla, nibh non vehicula tempus, massa velit semper dui, non ultrices justo est sit amet erat. Morbi varius quam nulla, a iaculis mauris dignissim at. Nunc in libero in dui tincidunt faucibus ut vitae massa. Quisque nec quam sed odio maximus pulvinar eu vitae felis. Ut interdum neque sed ornare porta. Ut ut felis a nisl tempor molestie in in tortor. Mauris venenatis, justo at venenatis rutrum, dolor turpis ornare nunc, in congue augue odio at mauris. Fusce at mollis massa. Fusce porttitor ullamcorper ante vestibulum pretium. Maecenas tellus ex, dapibus at gravida non, hendrerit in risus.</p>
+					<p>In my freetime I enjoy cycling, snowboarding, playing guitar, videogames, chasing my toddler around and obviously...Web Development</p>
 				</div>
 				<div class="col-6">
 					<div class="portrait"></div>
