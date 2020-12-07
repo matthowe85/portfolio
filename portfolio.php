13f1fa3c9960f1ea5e7329c45a1cecbecfe79ae4
MATTHEW HOWE <matthewhowe@MATTHEWs-iMac.local>
2020-12-07 09:28:32 -0500

    More stuff?

--- portfolio.php
+++ portfolio.php
@@ -10,29 +10,29 @@
 	<main id="swup" class="transition-fade">
 		<div id="page" class="not-front container portfolio-page container-fluid">
 			<div class="row">
-				<div class="col-4 card">
+				<div class="col-4 card ea">
 					<div class="screenshot"></div>
 					<div class="url"><a>Emerging America</a></div>
 				</div>
-				<div class="col-4 card">
+				<div class="col-4 card dys">
 					<div class="screenshot"></div>
 					<div class="url"><a>DYS Post Secondary Guides</a></div>
 				</div>
-				<div class="col-4 card">
+				<div class="col-4 card int">
 					<div class="screenshot"></div>
 					<div class="url"><a>CES Intranet</a></div>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-4 card">
+				<div class="col-4 card gmrsd">
 					<div class="screenshot"></div>
 					<div class="url"><a>Gill-Montague Regional School District</a></div>
 				</div>
-				<div class="col-4 card">
+				<div class="col-4 card bico">
 					<div class="screenshot"></div>
 					<div class="url"><a>Bi-County Collaborative</a></div>
 				</div>
-				<div class="col-4 card">
+				<div class="col-4 card ces">
 					<div class="screenshot"></div>
 					<div class="url"><a>Collaborative for Educational Services</a></div>
 				</div>
